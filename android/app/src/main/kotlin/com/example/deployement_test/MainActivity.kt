3f59b7d341e50e7eb0386fdbbcf462c50b2c5b9d
Lulupointu <lucasdelsol@hotmail.fr>
2022-12-13 14:56:14 +0100

    Start adding fastlane

--- android/app/src/main/kotlin/com/example/deployement_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/deployement_test/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.deployement_test
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
